d2d0baa78242b3dd24e5fac7e1739e62800da0f6
CodingRocks!! <ckhung5@gmail.com>
2016-12-22 09:38:14 +0800

    modified user: form, new , edit, controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ActiveRecord::Base
   validates_presence_of :first_name, :last_name
   validates :email, presence: true, uniqueness: true,
                     format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "Invalid, Should be xx@xx.com"}
+  has_secure_password
+  validates :password, presence: true, length: { in: 6..20 }
+
+
+
 
 end
