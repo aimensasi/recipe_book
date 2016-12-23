ca7c3b020f9a9217281c204cb3cc85d07f7f4f60
CodingRocks!! <ckhung5@gmail.com>
2016-12-23 18:23:20 +0800

    adjust roles

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,12 +2,12 @@ class WelcomeController < ApplicationController
 
     # before_action :new_user
 
-  layout "welcome_layout"
+  # layout "welcome_layout"
 
   def index
     if current_user
 
-      current_user
+      redirect_to current_user
     end
 
   end
