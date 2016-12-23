6b36b72e69649a1d7b97516071e649b1658df8b7
CodingRocks!! <ckhung5@gmail.com>
2016-12-24 00:50:36 +0800

    update the layouts, added some logics, all view files are tuned

---
+++ app/assets/images/RecipeBook_files/application.self-75303bbeab01983ed2ca55d1c8b55cdb3c537685f346a57fcfcb79fbb5cf88d7.js
@@ -0,0 +1,56 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+
+
+
+
+
+
+
+// $(document).ready(function(){
+
+//   $("#search_title").submit(function(event){
+
+//   // Search For events Using Full Text Search
+//   $eventSearch.on('submit', function(e){
+//     e.preventDefault();
+//     sendAjaxRequest("/title_search", $(this).serialize(), "search_title");
+//   });
+
+//   function sendAjaxRequest(url, data, action){
+//     if (action == SCHOOL_TYPE) {
+//       $schoolResult.empty();
+//     }else{
+//       $eventResult.empty();
+//     }
+//     $resultCover.removeClass('hide');
+
+//     $.ajax({
+//       url: url,
+//       type: 'GET',
+//       dataType: 'html',
+//       cache: false,
+//       data: data,
+//       success: function(data){
+//         updateView(data, action);
+//       },
+//       error: function(err){
+//         console.log("Error");
+//         console.log(err);
+//       }
+//     });
+//   } //AjaxFunction
+
+// });
+
+
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 
     # before_action :new_user
 
-  # layout "welcome_layout"
+  layout "welcome_layout"
 
   def index
     if current_user
